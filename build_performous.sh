8f92c39097497d2ff72817252583b59e5701d018
ooshlablu <ooshlablu@test.grarnsburg>
2021-07-21 23:43:37 -0400

    Update build script, add fedora to actions

--- build_performous.sh
+++ build_performous.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -ex
+## Pull in /etc/os-release so we can see what we're running on
+. /etc/os-release
+
+## All the git stuff
 git clone git://github.com/performous/performous.git
 cd performous
 if [ ${1} ]; then
@@ -7,16 +11,20 @@ if [ ${1} ]; then
 fi
 git submodule update --init --recursive
 
+## Set up some special cmake flags for fedora
+if [ ${ID} == 'fedora' ]; then
+  EXTRA_CMAKE_ARGS='-DUSE_BOOST_REGEX=1'
+fi
+
 ## Standard build
-mkdir build
-cd build
-cmake ..
+mkdir build.make
+cd build.make
+cmake ${EXTRA_CMAKE_ARGS} ..
 CPU_CORES=$(nproc --all)
 make -j${CPU_CORES}
 cd ..
 
 ## Meson build
-mv build build.make
 meson build
 cd build
 meson compile
